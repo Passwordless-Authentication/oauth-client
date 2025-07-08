254cd43e1f19f1788c09d6379f9d0b373b546592
Iliya Geramand <iliyageramand@gmail.com>
2025-07-08 15:40:41 +0330

    make session management stateless

--- src/main/kotlin/dev/iliya/passwordless/auth/oauthclient/config/SecurityConfiguration.kt
+++ src/main/kotlin/dev/iliya/passwordless/auth/oauthclient/config/SecurityConfiguration.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
@@ -44,7 +45,11 @@ class SecurityConfiguration(
                 authenticationSuccessHandler =
                     RedirectAuthenticationSuccessHandler(clientService, tokenService, redirectUri)
             }
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
         }
+
         return httpSecurity.build()
     }
 }
